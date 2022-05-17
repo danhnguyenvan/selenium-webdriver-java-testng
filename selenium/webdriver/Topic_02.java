f7488f9f101b6df553a99072417599ee20122080
Danh Nguyen Van <nguyendanh565.2013@gmail.com>
2022-05-17 11:38:40 +0700

    Create repository

---
+++ selenium/webdriver/Topic_02.java
@@ -0,0 +1,8 @@
+package webdriver;
+
+public class Topic_02 {
+	public static void main(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+	}
+}
