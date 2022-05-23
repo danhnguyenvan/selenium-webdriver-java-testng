14963f9d3384fe20be7c730da18a5a78837a6dd7
Danh Nguyen Van <nguyendanh565.2013@gmail.com>
2022-05-23 10:01:23 +0700

    Update 2 TCs of Login item for CreatTopic 05 Web Browser Command II

--- selenium/webdriver/Topic_05_Web_Browser_Command_II.java
+++ selenium/webdriver/Topic_05_Web_Browser_Command_II.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -14,11 +15,19 @@ import org.testng.annotations.Test;
 public class Topic_05_Web_Browser_Command_II {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
+	
+	String firstName, lastName, fullName, email, password;
 
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
+
+		firstName = "John";
+		lastName = "Wick";
+		fullName = firstName + " " + lastName;
+		email = "john" + getRandomNumber() + "@gmail.us" ;
+		password = "123456";
 		
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		
@@ -96,6 +105,62 @@ public class Topic_05_Web_Browser_Command_II {
 		
 		
 		
+	}
+	
+	@Test
+	public void TC_05_LiveGuru_Register() {
+		// Mở application lên (AUT/ SUT)
+		driver.get("http://live.techpanda.org/");
+		
+		// Click vào link "My Account"
+		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+		
+		// Click CREATE AN ACCOUNT button
+		driver.findElement(By.xpath("//a[@title='Create an Account' and @class='button']")).click();
+		
+		
+		driver.findElement(By.id("firstname")).sendKeys(firstName);
+		driver.findElement(By.id("lastname")).sendKeys(lastName);
+		driver.findElement(By.id("email_address")).sendKeys(email);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("confirmation")).sendKeys(password);
+		
+		driver.findElement(By.xpath("//button[@title='Register']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//li[@class='success-msg']//span[text()='Thank you for registering with Main Website Store.']")).isDisplayed());
+		
+		// Cách 1
+		String contactInformation = driver.findElement(By.xpath("//h3[text()='Contact Information']/parent::div/following-sibling::div[@class='box-content']/p")).getText();
+		Assert.assertTrue(contactInformation.contains(fullName));
+		Assert.assertTrue(contactInformation.contains(email));
+		
+		// Cách 2
+		Assert.assertTrue(driver.findElement(By.xpath("//h3[text()='Contact Information']"
+				+ "/parent::div/following-sibling::div[@class='box-content']/p[contains(string(),'"+ fullName +"')]")).isDisplayed());	
+		Assert.assertTrue(driver.findElement(By.xpath("//h3[text()='Contact Information']"
+				+ "/parent::div/following-sibling::div[@class='box-content']/p[contains(string(),'"+ email +"')]")).isDisplayed());	
+		
+		driver.findElement(By.xpath("//a/span[text()='Account']")).click();
+		driver.findElement(By.xpath("//a[@title='Log Out']")).click();
+	}
+	
+	@Test
+	public void TC_06_LiveGuru_Login() {
+		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+		
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.name("login[password]")).sendKeys(password);
+		driver.findElement(By.id("send2")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='My Dashboard']")).isDisplayed());
+		String contactInformation = driver.findElement(By.xpath("//h3[text()='Contact Information']/parent::div/following-sibling::div[@class='box-content']/p")).getText();
+		Assert.assertTrue(contactInformation.contains(fullName));
+		Assert.assertTrue(contactInformation.contains(email));
+
+	}
+	
+	public int getRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(99999);
 	}
 	
 	@AfterClass
