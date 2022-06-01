261037d92fa8a6bb0d8f7ae04956dac77c837a3a
Danh Nguyen Van <nguyendanh565.2013@gmail.com>
2022-06-01 10:17:59 +0700

    Change (Adding a new Topic 09 Alert) to (Topic 10 Handle Alert)

--- selenium/webdriver/Topic_10_Handle_Alert.java
+++ selenium/webdriver/Topic_10_Handle_Alert.java
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_09_Handle_Alert {
+public class Topic_10_Handle_Alert {
 	WebDriver driver;
 	Alert alert;
 	String projectPath = System.getProperty("user.dir");
